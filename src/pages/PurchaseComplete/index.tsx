f18b0ac73ef3701d812891f8dca798976d601833
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2024-07-03 10:20:49 -0300

    feat: PurchaseComplete page

---
+++ src/pages/PurchaseComplete/index.tsx
@@ -0,0 +1,5 @@
+import { PurchaseCompleted } from "../../components/PurchaseCompleted/PurchaseCompleted.styled";
+
+export default function PurchaseComplete() {
+    return <PurchaseCompleted />;
+}
