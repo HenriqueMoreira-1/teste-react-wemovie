e85ed7c3b2150b4084e597f8c96734f3e9e784f0
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2024-07-03 10:10:18 -0300

    chore: Install necessary dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,16 @@
-import React, { useEffect, useState } from "react";
-import Card from "./components/Card";
-import axios from "axios";
-import { AppContainer } from "./styles";
-
-interface MoviesResponse {
-  products: {
-    id: number;
-    title: string;
-    price: number;
-    image: string;
-  }[];
-}
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import routes from "./routes/routes";
 
 function App() {
-  const [dataResponse, setDataResponse] = useState<MoviesResponse | null>(null);
-  const [loading, setLoading] = useState(true);
+    const router = createBrowserRouter([
+        {
+            element: <Layout />,
+            children: routes,
+        },
+    ]);
 
-  const api = "https://wefit-movies.vercel.app/api/movies";
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(api);
-      setDataResponse(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!dataResponse) {
-    return <div>No data available</div>;
-  }
-
-  return (
-    <AppContainer>
-      {
-        React.Children.toArray(
-          dataResponse?.products.map((product) => (
-            <Card
-              title={product.title}
-              price={product.price}
-              image={product.image}
-            />
-          ))
-        )
-      }
-    </AppContainer>
-  );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/styles.ts
+++ src/styles.ts
@@ -6,4 +6,8 @@ export const AppContainer = styled.div`
     gap: 20px;
     width: 80%;
     margin: 0 auto;
+
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
 `;
